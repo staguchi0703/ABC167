7d05bfdea2403d7d4ec777321ddd7d840c4562ba
sho Taguchi <s.taguchi0703@gmail.com>
2020-05-11 21:49:44 +0900

    ABC157D

--- D/resolve.py
+++ D/resolve.py
@@ -2,36 +2,37 @@ def resolve():
     '''
     code here
     '''
+    import collections
     N, K = [int(item) for item in input().split()]
     A_list = [int(item) for item in input().split()]
     order_dict = dict(zip(range(1,N+1),A_list))
 
-    foot_print = []
+    foot_print = [-1 for _ in range(N+1)]
+    loop_que = collections.deque([1])
 
     goto = 1
-    for i in range(N):
-        if goto in foot_print:
-            break
-        else:
-            foot_print.append(goto)
-            goto = order_dict[goto]
 
-    if K > len(foot_print):
-        foot_print2 = []
-        for i in range(N):
-            if goto in foot_print2:
-                break
-            else:
-                foot_print2.append(goto)
-                goto = order_dict[goto]
+    while foot_print[goto] == -1:
+        foot_print[goto] = 1
+        goto = order_dict[goto]
+        loop_que.append(goto) 
+
+    loop_start = loop_que.index(goto) + 1
+    loop_len = len(loop_que) - loop_start
+
+    # print(loop_que)
+    # print(loop_start, loop_len)
 
 
+    data_list = list(loop_que)
+    if K >= len(loop_que):
+        temp_K = K - loop_start + 1
+        temp_K %= loop_len
+        temp_loop = data_list[loop_start-1:-1]
+        print(temp_loop[temp_K])
+    else:
 
-        temp_num = K -len(foot_print)
-        temp_num = temp_num % len(foot_print2)
-        for i in range(temp_num):
-            goto = order_dict[goto]
+        print(data_list[K])
 
-    print(goto)
 if __name__ == "__main__":
     resolve()
